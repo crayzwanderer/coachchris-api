4665254958b7e8fe0eeae911038aa35a14d93b17
Christina Velasquez <positivedistruction@gmail.com>
2025-11-14 12:56:41 -0600

    updated db.js

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "localhost",
+  host: "coachchris-api.up.railway.app/api/reviews",
   user: "coachuser",
   password: "coachpass123!",
   database: "coachdb",
