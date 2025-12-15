3f528645f19cd156027d2a1fc687e971acee94cb
Christina Velasquez <positivedistruction@gmail.com>
2025-12-15 15:26:53 -0600

    updated server.js

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.use(express.json());
 // MySQL connection pool
 // -----------------------------
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: Number(process.env.MYSQLPORT),
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: Number(process.env.MYSQL_PORT),
   waitForConnections: true,
   connectionLimit: 10,
 });
