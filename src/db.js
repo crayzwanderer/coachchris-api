07a0f612fba9210b64d66169d9f7a726ecdd3f9b
Christina Velasquez <positivedistruction@gmail.com>
2025-12-21 23:20:24 -0600

    update db.js

--- src/db.js
+++ src/db.js
@@ -1,16 +1,11 @@
-// db.js
-require("dotenv").config(); // Load environment variables from .env
-const mysql = require("mysql2/promise");
+import mysql from "mysql2";
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST, // e.g., "monorail.proxy.rlwy.net"
-  user: process.env.DB_USER, // e.g., "coachuser"
-  password: process.env.DB_PASSWORD, // e.g., "coachpass123!"
-  database: process.env.DB_NAME, // e.g., "coachdb"
-  port: process.env.DB_PORT || 3306, // Optional fallback to 3306
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
 });
 
-module.exports = { pool };
+export default pool.promise();
